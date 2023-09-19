b043fd59d9c27764f7d7b9136be51f53ab110536
Asish Tammana <tammanaasish@gmail.com>
2023-09-19 11:51:29 +0000

    Bajaj Finserv Node By Asish Tammana

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server Running at http://localhost:3000/");
